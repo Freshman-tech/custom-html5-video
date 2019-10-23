911e6fbfc39d670cb26e94d6f3013b9426f4a679
Ayooluwa Isaiah <ayisaiah@gmail.com>
2019-10-23 12:10:20 +0100

    Updates

--- index.js
+++ index.js
@@ -1,33 +1,126 @@
-const videoContainer = document.getElementById('video-container');
+// Select elements here
 const video = document.getElementById('video');
 const videoControls = document.getElementById('video-controls');
+const playButton = document.getElementById('play');
+const playbackIcons = document.querySelectorAll('.playback-icons use');
+const timeElapsed = document.getElementById('time-elapsed');
+const duration = document.getElementById('duration');
 const progressBar = document.getElementById('progress-bar');
 const seek = document.getElementById('seek');
 const seekTooltip = document.getElementById('seek-tooltip');
-const playButton = document.getElementById('play');
-const playbackIcons = document.querySelectorAll('.playback-icons use');
 const volumeButton = document.getElementById('volume-button');
 const volumeIcons = document.querySelectorAll('.volume-button use');
 const volumeMute = document.querySelector('use[href="#volume-mute"]');
 const volumeLow = document.querySelector('use[href="#volume-low"]');
 const volumeHigh = document.querySelector('use[href="#volume-high"]');
 const volume = document.getElementById('volume');
-const currentTime = document.getElementById('current-time');
-const totalTime = document.getElementById('total-time');
 const playbackAnimation = document.getElementById('playback-animation');
 const fullscreenButton = document.getElementById('fullscreen-button');
+const videoContainer = document.getElementById('video-container');
+const fullscreenIcons = fullscreenButton.querySelectorAll('use');
 const pipButton = document.getElementById('pip-button')
 
-function formatTime (timeInSeconds) {
+const videoWorks = !!document.createElement('video').canPlayType;
+if (videoWorks) {
+  video.controls = false
+  videoControls.classList.remove('hidden');
+}
+
+// Add functions here
+
+// togglePlay toggles the playback state of the video.
+// If the video playback is paused or ended, the video is played
+// otherwise, the video is paused
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// updatePlayButton updates the playback icon and tooltip
+// depending on the playback state
+function updatePlayButton() {
+  playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+
+  if (video.paused) {
+    playButton.setAttribute('data-title', 'Play (k)')
+  } else {
+    playButton.setAttribute('data-title', 'Pause (k)')
+  }
+}
+
+// formatTime takes a time length in seconds and returns the time in
+// minutes and seconds
+function formatTime(timeInSeconds) {
   const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
 
   return {
-    hours: result.substr(0, 2),
     minutes: result.substr(3, 2),
     seconds: result.substr(6, 2),
   };
 };
 
+// initializeVideo sets the video duration, and maximum value of the
+// progressBar
+function initializeVideo() {
+  const videoDuration = Math.round(video.duration);
+  seek.setAttribute('max', videoDuration);
+  progressBar.setAttribute('max', videoDuration);
+  const time = formatTime(videoDuration);
+  duration.innerText = `${time.minutes}:${time.seconds}`;
+  duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+// updateTimeElapsed indicates how far through the video
+// the current playback is by updating the timeElapsed element
+function updateTimeElapsed() {
+  const time = formatTime(Math.round(video.currentTime));
+  timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+  timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+// updateProgress indicates how far through the video
+// the current playback is by updating the progress bar
+function updateProgress() {
+  seek.value = Math.floor(video.currentTime);
+  progressBar.value = Math.floor(video.currentTime);
+}
+
+// updateSeekTooltip uses the position of the mouse on the progress bar to
+// roughly work out what point in the video the user will skip to if
+// the progress bar is clicked at that point
+function updateSeekTooltip(event) {
+  const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
+  seek.setAttribute('data-seek', skipTo)
+  const t = formatTime(skipTo);
+  seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+  const rect = video.getBoundingClientRect();
+  seekTooltip.style.left = `${event.pageX - rect.left}px`;
+}
+
+// skipAhead jumps to a different point in the video when the progress bar
+// is clicked
+function skipAhead(event) {
+  const skipTo = event.target.dataset.seek;
+  video.currentTime = skipTo;
+  progressBar.value = skipTo;
+  seek.value = skipTo;
+}
+
+// updateVolume updates the video's volume
+// and disables the muted state if active
+function updateVolume() {
+  if (video.muted) {
+    video.muted = false;
+  }
+
+  video.volume = volume.value;
+}
+
+// updateVolumeIcon updates the volume icon so that it correctly reflects
+// the volume of the video
 function updateVolumeIcon() {
   volumeIcons.forEach(icon => {
     icon.classList.add('hidden');
@@ -45,30 +138,26 @@ function updateVolumeIcon() {
   }
 }
 
-function togglePlay(event) {
-  if (video.paused || video.ended) {
-    video.play();
+// toggleMute mutes or unmutes the video when executed
+// When the video is unmuted, the volume is returned to the value
+// it was set to before the video was muted
+function toggleMute() {
+  video.muted = !video.muted;
+
+  if (video.muted) {
+    volume.setAttribute('data-volume', volume.value);
+    volume.value = 0;
   } else {
-    video.pause();
+    volume.value = volume.dataset.volume;
   }
 }
 
-function hideControls() {
-  if (video.paused) {
-    return;
-  }
-
-  videoControls.classList.add('hide');
-}
-
-function showControls() {
-  videoControls.classList.remove('hide');
-}
-
+// animatePlayback displays an animation when
+// the video is played or paused
 function animatePlayback() {
     playbackAnimation.animate([
       {
-        opacity: 0.6,
+        opacity: 1,
         transform: "scale(1)",
       },
       {
@@ -80,29 +169,20 @@ function animatePlayback() {
     });
 }
 
-function updatePlayButton(event) {
-  playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
-
-  if (video.paused) {
-    playButton.setAttribute('data-title', 'Play (k)')
+// toggleFullScreen toggles the full screen state of the video
+// If the browser is currently in fullscreen mode,
+// then it must be exited and vice versa.
+function toggleFullScreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
   } else {
-    playButton.setAttribute('data-title', 'Pause (k)')
+    videoContainer.requestFullscreen();
   }
 }
 
-function toggleMute() {
-  video.muted = !video.muted;
-
-  if (video.muted) {
-    volume.setAttribute('data-mute', volume.value);
-    volume.value = 0;
-  } else {
-    volume.value = volume.dataset.mute;
-  }
-}
-
-function updateFullScreenButton() {
-  const fullscreenIcons = document.querySelectorAll('.js-fullscreen-button use');
+// updateFullscreenButton changes the icon of the full screen button
+// and tooltip to reflect the current full screen state of the video
+function updateFullscreenButton() {
   fullscreenIcons.forEach(icon => icon.classList.toggle('hidden'));
 
   if (document.fullscreenElement) {
@@ -112,73 +192,39 @@ function updateFullScreenButton() {
   }
 }
 
-function toggleFullScreen() {
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    videoContainer.requestFullscreen();
+// togglePip toggles Picture-in-Picture mode on the video
+async function togglePip() {
+  try {
+    if (video !== document.pictureInPictureElement) {
+      pipButton.disabled = true;
+      await video.requestPictureInPicture();
+    } else {
+      await document.exitPictureInPicture();
+    }
+  } catch (error) {
+    console.error(error)
+  } finally {
+    pipButton.disabled = false;
   }
 }
 
-function updateVolume() {
-  if (video.muted) {
-    video.muted = false;
+// hideControls hides the video controls when not in use
+// if the video is paused, the controls must remain visible
+function hideControls() {
+  if (video.paused) {
+    return;
   }
 
-  video.volume = volume.value;
+  videoControls.classList.add('hide');
 }
 
-function updatePlayback() {
-  const dataPosition = event.target.dataset.position;
-  video.currentTime = dataPosition;
-  progressBar.value = dataPosition;
-  seek.value = dataPosition;
-  progressBar.value = dataPosition;
+// showControls displays the video controls
+function showControls() {
+  videoControls.classList.remove('hide');
 }
 
-function initializeVideo() {
-  const videoDuration = Math.round(video.duration);
-  seek.setAttribute('max', videoDuration);
-  progressBar.setAttribute('max', videoDuration);
-  const duration = formatTime(videoDuration);
-  totalTime.innerText = `${duration.minutes}:${duration.seconds}`;
-  totalTime.setAttribute('datetime', `${duration.minutes}m ${duration.seconds}s`)
-}
-
-function updateProgress() {
-  if (!progressBar.getAttribute('max')) {
-    progressBar.setAttribute('max', video.duration);
-  }
-  seek.value = Math.floor(video.currentTime);
-  progressBar.value = Math.floor(video.currentTime);
-}
-
-function updateCurrentTime() {
-  const duration = formatTime(Math.round(video.currentTime));
-  currentTime.innerText = `${duration.minutes}:${duration.seconds}`;
-  currentTime.setAttribute('datetime', `${duration.minutes}m ${duration.seconds}s`)
-}
-
-function updateSeekTooltip(event) {
-  const hoverTime = (event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10);
-  seek.setAttribute('data-position', hoverTime)
-  const t = formatTime(hoverTime);
-  seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
-  const rect = video.getBoundingClientRect();
-  seekTooltip.style.left = `${event.pageX - rect.left}px`;
-}
-
-function togglePip() {
-  if (video !== document.pictureInPictureElement) {
-    pipButton.disabled = true;
-    return video.requestPictureInPicture()
-      .catch(console.error)
-      .finally(() => pipButton.disabled = false);
-  }
-
-  document.exitPictureInPicture();
-}
-
+// keyboardShortcuts executes the relevant functions for
+// each supported shortcut key
 function keyboardShortcuts(event) {
   const { key } = event;
   switch(key) {
@@ -205,41 +251,31 @@ function keyboardShortcuts(event) {
   }
 }
 
-playButton.onclick = togglePlay;
-volumeButton.onclick = toggleMute;
-fullscreenButton.onclick = toggleFullScreen;
-pipButton.onclick = togglePip;
-
-videoContainer.onfullscreenchange = updateFullScreenButton;
-
-volume.oninput = updateVolume;
-
-video.onloadedmetadata = initializeVideo;
-video.onplay = updatePlayButton;
-video.onpause = updatePlayButton;
-video.onvolumechange = updateVolumeIcon;
-video.onmouseenter = showControls;
-video.onmouseover = showControls;
-video.onmouseleave = hideControls;
-video.ontimeupdate = () => {
-  updateProgress();
-  updateCurrentTime();
-};
-video.onclick = () => {
-  togglePlay()
-  animatePlayback();
-};
-
-videoControls.onmouseleave = hideControls;
-videoControls.onmouseover = showControls;
-
-seek.oninput = updatePlayback;
-seek.onmousemove = updateSeekTooltip;
-
-document.onkeyup = keyboardShortcuts;
+// Add eventlisteners here
+playButton.addEventListener('click', togglePlay);
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+video.addEventListener('loadedmetadata', initializeVideo);
+video.addEventListener('timeupdate', updateTimeElapsed);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('volumechange', updateVolumeIcon);
+video.addEventListener('click', togglePlay);
+video.addEventListener('click', animatePlayback);
+video.addEventListener('mouseenter', showControls);
+video.addEventListener('mouseleave', hideControls);
+videoControls.addEventListener('mouseenter', showControls);
+videoControls.addEventListener('mouseleave', hideControls);
+seek.addEventListener('mousemove', updateSeekTooltip);
+seek.addEventListener('input', skipAhead);
+volume.addEventListener('input', updateVolume);
+volumeButton.addEventListener('click', toggleMute);
+fullscreenButton.addEventListener('click', toggleFullScreen);
+videoContainer.addEventListener('fullscreenchange', updateFullscreenButton);
+pipButton.addEventListener('click', togglePip);
 
 document.addEventListener('DOMContentLoaded', () => {
   if (!('pictureInPictureEnabled' in document)) {
     pipButton.classList.add('hidden');
   }
 });
+document.addEventListener('keyup', keyboardShortcuts);
